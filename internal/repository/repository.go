04c831d4094af28094f86325b1ae6c93c9cfa42a
yongckim <zxcv9203@naver.com>
2023-07-02 00:16:11 +0900

    :sparkles: 데이터 접근시 사용되는 인터페이스 정의

---
+++ internal/repository/repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"github.com/zxcv9203/fiber-post/internal/model"
+	"github.com/zxcv9203/fiber-post/pkg/page"
+)
+
+type repository interface {
+	findById(id int64) (model.Post, error)
+	save(post model.Post) (model.Post, error)
+	update(id int64, post model.Post) (model.Post, error)
+	findAll(request page.Request) ([]model.Post, error)
+	deleteById(id int64) error
+}
