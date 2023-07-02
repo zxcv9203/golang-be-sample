335a4827fb51e85f735179d01367ce11b2de4174
yongckim <zxcv9203@naver.com>
2023-07-02 22:34:39 +0900

    :sparkles: fiber를 이용하여 웹 애플리케이션 실행

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	port := flag.String("p", "8080", "애플리케이션을 지정할 포트를 지정합니다.")
+	flag.Parse()
+
+	startFiber(*port)
+}
+
+func startFiber(port string) {
+	app := fiber.New()
+
+	err := app.Listen(":" + port)
+	if err != nil {
+		log.Panicln(err)
+	}
 }
